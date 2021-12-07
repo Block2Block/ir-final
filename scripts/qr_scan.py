f57d3bb67c7f51fb91b53045002ef547211de757
antoniehuang <antonieyuchi@gmail.com>
2021-12-07 20:25:03 +0000

    updated qr_scan location information to store x,y cord as tuples

--- scripts/qr_scan.py
+++ scripts/qr_scan.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, PoseArray, Quaternion
+import math
 
 THRESH = 40     #threshold for image
 
@@ -22,10 +23,22 @@ def increase_brightness(image, value=10):
     return image
 
 product_loc_dict = dict()
-loc_product = Pose()
+
+def round_nearest2(x, a):
+    return round(round(x / a) * a, -int(math.floor(math.log10(a))))
 
 def get_pose(msg):
+    #print(f"msg {msg}")
+    global loc_product
+    loc_product = Pose()
     loc_product = msg.pose.pose
+
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    
+    # This has been rounded to the nearest to 0.03, as our dijkstra grid system is implemented with 0.03 stepping stone.
+    global xy_tuple 
+    xy_tuple = (round_nearest2(x, 0.03), round_nearest2(y, 0.03))
 
 def img_callback(imgdata):
     img_bridge = CvBridge()
@@ -37,21 +50,17 @@ def img_callback(imgdata):
         median_blur= cv2.medianBlur(imgbw, 5)       #to remove salt and pepper noise
         qrdata = decode(imgbw)      #extracting qr code information
         if qrdata:
-            #print(f"qrdata {qrdata}")
             qrinfo = qrdata[0].data
-            #print(qrinfo)
-            #type(qrinfo)
             if qrinfo not in product_loc_dict.keys():
                 product_loc_dict[qrinfo] = list()
-                product_loc_dict[qrinfo].append(loc_product)
+                product_loc_dict[qrinfo].append(xy_tuple)
                 print(product_loc_dict)
-            #Since QR information is present we record the loc
-            odom_sub = rospy.Subscriber('/odom', Odometry, callback=get_pose)
                 
     except CvBridgeError as e:
         rospy.logerr(e)
-    
 
 rospy.init_node('qr_scan_node')
 sub = rospy.Subscriber("/turtlebot3_burger/camera1/image_raw", Image, callback=img_callback)  # Subscriber object which will listen "LaserScan" type messages from the "/scan" Topic and call the "callback" function each time it reads something from the Topic
+#Since QR information is present we record the loc
+odom_sub = rospy.Subscriber('/odom', Odometry, callback=get_pose)
 rospy.spin() # Loops infinitely until someone stops the program execution
